e95b5761b1792e03ea687e2f5751b1387b218cb8
thakkaralpesh <thakkaralpesh@gmail.com>
2024-08-14 21:33:09 +0530

    first part of API

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext context) : ControllerBase //tip: use primary constructor c# 12 new concept
+{
+    private readonly DataContext _context = context;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUserById(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+
+        if(user == null) return NotFound();
+
+        return user;
+    }
+}   
